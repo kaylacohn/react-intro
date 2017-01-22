024655d3e37ffd5d2080577d4dd6a13dac26518a
Kayla Cohn <kayla.cohn@salesloft.com>
2017-01-21 19:10:50 -0500

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+
+module.exports = {
+  context: __dirname,
+  entry: './js/ClientApp.js',
+  devtool: 'eval',
+  output: {
+    path: path.join(__dirname, '/public'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    publicPath: '/public/'
+  },
+  resolve: {
+    extensions: ['.js', '.json']
+  },
+  stats: {
+    colors: true,
+    reasons: true,
+    chunks: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              url: false
+            }
+          }
+        ]
+      }
+    ]
+  }
+}
