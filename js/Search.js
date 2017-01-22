4f2296f3fc3ef0ae472c32f8a0cbf3640e865d36
Kayla Cohn <kayla.cohn@salesloft.com>
2017-01-22 15:05:25 -0500

    Testing components

--- js/Search.js
+++ js/Search.js
@@ -3,14 +3,33 @@ import preload from '../public/data.json'
 import ShowCard from './ShowCard'
 
 const Search = React.createClass({
+  getInitialState() {
+    return {
+      searchTerm: ''
+    }
+  },
+  handleSearchTermChange (event) {
+     this.setState({ searchTerm: event.target.value })
+  },
   render() {
     return (
       <div className="search">
-        { preload.shows.map((show) => {
-          return (
-            <ShowCard key = { show.imdbID } show = { show }/>
-          );
-        })};
+        <header>
+          <h1>VidFlix</h1>
+          <input type="text" placeholder = "Search" value = { this.state.searchTerm } onChange = { this.handleSearchTermChange } />
+        </header>
+        <div>
+          { preload.shows
+            .filter((show) => {
+              return `${ show.title } ${ show.description }`.
+              toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0
+            })
+            .map((show) => {
+            return (
+              <ShowCard key = { show.imdbID } { ...show }/>
+            );
+          })};
+        </div>
       </div>
     );
   }
